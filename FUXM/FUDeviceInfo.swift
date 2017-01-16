ed3e9e4121225d04e2350885dca12e1ed4f11f86
Luis Wu <dongyi.wu@honestbee.com>
2017-01-16 16:38:41 +0800

    Revert FUDeviceInfo

--- FUXM/FUDeviceInfo.swift
+++ FUXM/FUDeviceInfo.swift
@@ -13,13 +13,9 @@ class FUDeviceInfo: NSObject {
     let salt: UInt8
     let profileVer: String
     let firmwareVer: String
-    let feature: Int
-    let appearance: Int
-    let hardwareVer: Int
-    
-    // heart rate firmware version identifier
-    let firmware2Ver: Int32
-    let test1AHeartRateMode: Bool
+    let feature: UInt
+    let appearance: UInt
+    let hardwareVer: UInt
     
     override var debugDescription: String {
         return "deviceID: \(deviceID), "
@@ -31,7 +27,7 @@ class FUDeviceInfo: NSObject {
             + "appearance: \(appearance), "
             + "hardwareVer: \(hardwareVer)"
     }
-    
+
     private struct Consts {
         static let deviceIDRange: Range<Data.Index> = 0..<7
         static let macFormatString = "%02x:%02x:%02x:%02x:%02x:%02x"
@@ -49,93 +45,51 @@ class FUDeviceInfo: NSObject {
         static let firmwareVerMinorOffset = 1
         static let firmwareVerBuildOffset = 2
         static let firmwareVerRevisionOffset = 3
-        static let firmwareVer2Range: Range<Data.Index> = 16..<20
         static let featureRange: Range<Data.Index> = 4..<5
         static let appearanceRange: Range<Data.Index> = 5..<6
         static let hardwareVerRange: Range<Data.Index> = 6..<7
         static let verFormatString = "%d.%d.%d.%d"
-        
-        static let validDataLength16 = 16
-        static let validDataLength20 = 20
     }
     
     required init?(data: Data?) {   // failable initializer
         guard let data = data else {
             return nil
         }
-        
-        
-        if (data.count == Consts.validDataLength16
-            || data.count == Consts.validDataLength20)
-            && FUDeviceInfo.isChecksumCorrect(data: data) {
-            let deviceIDInInt32 = data.subdata(in: Consts.deviceIDRange).withUnsafeBytes({(pointer: UnsafePointer<Int8>) -> Int32 in
-                pointer.withMemoryRebound(to: Int32.self, capacity: MemoryLayout<Int32>.size) { pointer in
-                    return pointer.pointee
-                }
-            })
-            deviceID = String(deviceIDInInt32.bigEndian,
-                              radix:GlobalConsts.hexRadix,
-                              uppercase: false
-            )
-            mac = data.subdata(in: Consts.deviceIDRange).withUnsafeBytes { (pointer: UnsafePointer<Int8>) -> String in
-                return String(format: Consts.macFormatString, pointer.pointee,
-                              Consts.fixedMACAddressSection2,
-                              Consts.fixedMACAddressSection3,
-                              pointer.advanced(by: Consts.section4MACOffset).pointee,
-                              pointer.advanced(by: Consts.section5MACOffset).pointee,
-                              pointer.advanced(by: Consts.section6MACOffset).pointee)
+        let deviceIDInUInt32 = data.subdata(in: Consts.deviceIDRange).withUnsafeBytes({(pointer: UnsafePointer<UInt8>) -> UInt32 in
+            pointer.withMemoryRebound(to: UInt32.self, capacity: MemoryLayout<UInt32>.size) { pointer in
+                return pointer.pointee
             }
-            salt = data.subdata(in: Consts.deviceIDRange).withUnsafeBytes {return $0.advanced(by: Consts.saltOffset).pointee}
-            profileVer = Data(bytes: data.subdata(in: Consts.profileVerRange).reversed())
-                .withUnsafeBytes({ (pointer: UnsafePointer<Int8>) -> String in
-                    String(format:Consts.verFormatString, pointer.pointee,   // major
-                        pointer.advanced(by: Consts.profileVerMinorOffset).pointee,
-                        pointer.advanced(by: Consts.profileVerBuildOffset).pointee,
-                        pointer.advanced(by: Consts.profileVerRevisionOffset).pointee)
-                })
-            firmwareVer = Data(bytes: data.subdata(in: Consts.firmwareVerRange).reversed())
-                .withUnsafeBytes({ (pointer: UnsafePointer<Int8>) -> String in
-                    String(format:Consts.verFormatString, pointer.pointee,   // major
-                        pointer.advanced(by: Consts.firmwareVerMinorOffset).pointee,
-                        pointer.advanced(by: Consts.firmwareVerBuildOffset).pointee,
-                        pointer.advanced(by: Consts.firmwareVerRevisionOffset).pointee)
-                })
-            feature = data.subdata(in: Consts.featureRange).withUnsafeBytes { return $0.pointee }
-            appearance = data.subdata(in: Consts.appearanceRange).withUnsafeBytes { return $0.pointee }
-            hardwareVer = data.subdata(in: Consts.hardwareVerRange).withUnsafeBytes { return $0.pointee }
-            
-            if (data.count == Consts.validDataLength20) {
-                firmware2Ver = data.subdata(in: Consts.firmwareVer2Range).withUnsafeBytes( { return ($0 as UnsafePointer<Int32>).pointee } )
-            } else {
-                firmware2Ver = -1
-            }
-            test1AHeartRateMode = false
-        } else {
-            return nil
+        })
+        deviceID = String(deviceIDInUInt32.bigEndian,
+                          radix:GlobalConsts.hexRadix,
+                          uppercase: false
+        )
+        mac = data.subdata(in: Consts.deviceIDRange).withUnsafeBytes { (pointer: UnsafePointer<UInt8>) -> String in
+            return String(format: Consts.macFormatString, pointer.pointee,
+                          Consts.fixedMACAddressSection2,
+                          Consts.fixedMACAddressSection3,
+                          pointer.advanced(by: Consts.section4MACOffset).pointee,
+                          pointer.advanced(by: Consts.section5MACOffset).pointee,
+                          pointer.advanced(by: Consts.section6MACOffset).pointee)
         }
+        salt = data.subdata(in: Consts.deviceIDRange).withUnsafeBytes {return $0.advanced(by: Consts.saltOffset).pointee}
+        profileVer = Data(bytes: (data.subdata(in: Consts.profileVerRange).reversed() as [UInt8]))
+            .withUnsafeBytes({ (pointer: UnsafePointer<UInt8>) -> String in
+                String(format:Consts.verFormatString, pointer.pointee,   // major
+                       pointer.advanced(by: Consts.profileVerMinorOffset).pointee,
+                       pointer.advanced(by: Consts.profileVerBuildOffset).pointee,
+                       pointer.advanced(by: Consts.profileVerRevisionOffset).pointee)
+        })
+        firmwareVer = Data(bytes: (data.subdata(in: Consts.firmwareVerRange).reversed() as [UInt8]))
+            .withUnsafeBytes({ (pointer: UnsafePointer<UInt8>) -> String in
+                String(format:Consts.verFormatString, pointer.pointee,   // major
+                       pointer.advanced(by: Consts.firmwareVerMinorOffset).pointee,
+                       pointer.advanced(by: Consts.firmwareVerBuildOffset).pointee,
+                       pointer.advanced(by: Consts.firmwareVerRevisionOffset).pointee)
+        })
+        feature = data.subdata(in: Consts.featureRange).withUnsafeBytes { return $0.pointee }
+        appearance = data.subdata(in: Consts.appearanceRange).withUnsafeBytes { return $0.pointee }
+        hardwareVer = data.subdata(in: Consts.hardwareVerRange).withUnsafeBytes { return $0.pointee }
     }
     
-    func isMili1() -> Bool {
-        return hardwareVer == 2
-    }
-    
-    func isMili1A() -> Bool {
-        return feature == 5 && appearance == 0 || feature == 0 && hardwareVer == 208
-    }
-    
-    func isMili1S() -> Bool {   // with LED heart rate sensor
-        // TODO: this is probably not quite correct, but hopefully sufficient for early 1S support
-        return (feature == 4 && appearance == 0) || hardwareVer == 4
-    }
-    
-    func supportHeartRate() -> Bool {
-        return isMili1S() || test1AHeartRateMode && isMili1A()
-    }
-    
-    // MARK - private methods
-    private static func isChecksumCorrect(data: Data?) -> Bool {
-        guard let data = data else { return false }
-        let crc8 = FUCRC8Util.crc8WithBytes(bytes: [UInt8](data), length: data.count)
-        return (data.withUnsafeBytes( { return $0.advanced(by: 7).pointee }) & 0xff) == (crc8 ^ data.withUnsafeBytes( { return $0.advanced(by: 3).pointee }) & 0xff)
-    }
 }
